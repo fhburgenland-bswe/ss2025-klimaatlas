bb01910f389eaf5a555ce9f795f3442ff1fa26d8
manuel <mn07@gmx.at>
2025-03-25 17:37:46 +0100

    feat: add initial project structure, backend with SpringBoot, frontend with Angular

---
+++ backend/src/main/java/at/big5health/klimaatlas/KlimaatlasApplication.java
@@ -0,0 +1,13 @@
+package at.big5health.klimaatlas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimaatlasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimaatlasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/at/big5health/klimaatlas/KlimaatlasApplicationTests.java
@@ -0,0 +1,13 @@
+package at.big5health.klimaatlas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimaatlasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
