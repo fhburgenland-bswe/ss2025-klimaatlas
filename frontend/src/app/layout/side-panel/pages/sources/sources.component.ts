24647f0585ae31ffd23ff95a9a14feccd13599db
abelhorvath04 <abelhorvath04@gmail.com>
2025-06-12 17:33:53 +0200

    feat: add new requirements from big5health

--- frontend/src/app/layout/side-panel/pages/sources/sources.component.ts
+++ frontend/src/app/layout/side-panel/pages/sources/sources.component.ts
@@ -11,6 +11,7 @@ export class SourcesComponent {
   sources = [
     { url: 'https://www.openstreetmap.org/#map=8/47.184/19.509', label: 'OpenStreetMap' },
     { url: 'https://data.hub.geosphere.at/', label: 'Geosphere'},
-    { url: 'https://data.hub.geosphere.at/dataset/spartacus-v2-1d-1km', label: 'Spartacus' }
+    { url: 'https://data.hub.geosphere.at/dataset/spartacus-v2-1d-1km', label: 'Spartacus' },
+    { url: 'https://api.gbif.org/', label: 'GBIF' }
   ];
 }
